a07c2b7ed46f36ef62ac7d43da3391f619645f8c
That Nguyen <nt7nguyen@gmail.com>
2025-04-23 12:24:13 +0200

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-import openrouteservice
-
-# Set up the client with your API key
-client = openrouteservice.Client(key='5b3ce3597851110001cf6248c6bb4d57491245129ea3e4cfad96b48f')  # Replace with your API key
